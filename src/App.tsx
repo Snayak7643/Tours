bacc4f6dee088174f98aaeb6bd8535de90668297
Snayak7643 <shubhamnayak7643@gmail.com>
2023-01-06 13:18:29 +0530

    Fetching Complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const url = "https://course-api.com/react-tours-project";
+
+  const [tours, setTours] = useState<object[]>([]);
+
+  const fetching = async () => {
+    const response = await fetch(url);
+    const res = await response.json();
+    setTours(res);
+    console.log(res);
+  };
+
+  useEffect(() => {
+    fetching();
+  }, []);
+
   return <h1>Tours</h1>;
 }
 
